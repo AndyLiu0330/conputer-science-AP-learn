426d01b704baab664d9889e474c4123c41aecd78
AndyLiu0330 <andyliu0330@gmail.com>
2023-04-07 15:00:23 -0700

    the grade will only count when you press A,B,C,D,F not others.

--- CheckTheGrade.java
+++ CheckTheGrade.java
@@ -7,22 +7,33 @@ public class CheckTheGrade {
       Scanner input = new Scanner(System.in);
       System.out.println("Enter the grades:");
       String grades = input.nextLine();
+      
+      
+      Scanner nameScanner  = new Scanner(System.in);
+      System.out.println("Enter your name:");
+      String name = nameScanner.nextLine();
+
       if (grades.equals("A")){
-      System.out.println("Excellent!");
+      System.out.println("Hi " + name + " .  " + " your grade is " + "Excellent!" ); 
       }
       else if (grades.equals("B")){
-         System.out.println("Good");
 
+      System.out.println("Hi " + name + " . " +" your grade is "+ "Good" ); 
       } else if (grades.equals("C")){
-         System.out.println("Poor");
+      System.out.println("Hi " + name + " . "+" your grade is " + "Poor" ); 
       }
 
        else if (grades.equals("F")){
-         System.out.println("Eregious");
+      System.out.println("Hi " + name + " . "+" your grade is " + "Eregious" ); 
       }
       else {
          System.out.println("Invald grade");
       }
+
+
+      input.close();
+      nameScanner.close();
+
 
       
 
