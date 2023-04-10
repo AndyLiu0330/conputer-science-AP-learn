02e2f3d65d3c0741cdecb061e2c99d81d7807428
AndyLiu0330 <andyliu0330@gmail.com>
2023-04-10 11:50:44 -0700

    Change one

--- CalsiusToFahreheit.java
+++ CalsiusToFahreheit.java
@@ -3,21 +3,6 @@ import java.util.Scanner;
 
 
 public class CalsiusToFahreheit {
-
-
-
-
-   public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-
-
-    System.out.print("Enter a degree in Calcus:");
-    double  calsius = input.nextDouble();
-
-    double fahrenheit = calsius * 9.0 / 5.0 + 32.0;
-    System.out.println("Calsius " + calsius + " is " + fahrenheit + " in fahrenhit");
-    input.close();  
-   } 
    public void CtoF(){
 
     Scanner input = new Scanner(System.in);
