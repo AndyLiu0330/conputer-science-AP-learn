eadfcaf0066d5b9298e21e7e7722962ccbaa3f8c
AndyLiu0330 <andyliu0330@gmail.com>
2023-04-10 11:48:07 -0700

    用Changedegrees去改變度F到度C，或度C到度F。

---
+++ Changedegrees.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Changedegrees {
+    public static void main(String[] args){
+        Scanner CF = new Scanner(System.in);
+        System.out.println("[0] Fahreheit to Calsius [1] Calsius to Fahrenheit Type 0 or 1:");
+        int a = CF.nextInt();
+
+        if ( a == 1 ){
+           FahrenheitToCalsius FTC = new FahrenheitToCalsius();
+           FTC.FtoC();  
+        }
+        else {
+            CalsiusToFahreheit CTF = new CalsiusToFahreheit();
+            CTF.CtoF();
+
+        }
+
+        CF.close();
+    }
+    
+}       
---
+++ FahrenheitToCalsius.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+
+
+public class FahrenheitToCalsius {
+   public static void main(String[] args){
+     
+    Scanner input = new Scanner(System.in);
+
+
+    System.out.print("Enter a degree in Fahrenheit:");
+    double Fahrenheit = input.nextDouble();
+
+    double calsius = (5.0 / 9) * (Fahrenheit - 32);
+    System.out.println("Fahrenheit" + Fahrenheit + " is " + calsius + "in calsius");
+    input.close();
+   }   
+   public void FtoC() {
+
+    Scanner input = new Scanner(System.in);
+
+
+    System.out.print("Enter a degree in Fahrenheit:");
+    double Fahrenheit = input.nextDouble();
+
+    double calsius = (5.0 / 9) * (Fahrenheit - 32);
+    System.out.println("Fahrenheit" + Fahrenheit + " is " + calsius + "in calsius");
+    input.close();
+
+   }
+}
